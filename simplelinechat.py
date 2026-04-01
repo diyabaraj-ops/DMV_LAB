9bbb60c3c363b6ff634653a1ed4d4a3bcf1392e2
diyabaraj-ops <diyabaraj@gmail.com>
2026-04-01 15:22:02 +0530

    Add files via upload

--- simplelinechat.py
+++ simplelinechat.py
@@ -1,13 +1,12 @@
 import matplotlib.pyplot as plt
 
 x = [1, 2, 3, 4, 5]
-y = [10, 20, 25, 30, 40]
+y = [10, 20, 15, 30, 25]
 
-plt.scatter(x, y)
+plt.plot(x, y)
 
-plt.title("Scatter Plot")
+plt.title("Simple Line Chart")
 plt.xlabel("X-axis")
 plt.ylabel("Y-axis")
 
 plt.show()
-
--- stairplot.py
+++ stairplot.py
@@ -1,13 +1,12 @@
 import matplotlib.pyplot as plt
 
 x = [1, 2, 3, 4, 5]
-y = [10, 20, 25, 30, 40]
+y = [2, 3, 5, 7, 11]
 
-plt.scatter(x, y)
+plt.step(x, y)
 
-plt.title("Scatter Plot")
+plt.title("Stair Plot")
 plt.xlabel("X-axis")
 plt.ylabel("Y-axis")
 
 plt.show()
-
